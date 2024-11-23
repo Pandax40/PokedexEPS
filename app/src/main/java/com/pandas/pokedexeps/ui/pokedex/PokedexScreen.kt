e91069a1dc4a95ff7f5ccfb7c66c3b51350c1435
Pandax40 <pandaxsantin@gmail.com>
2024-11-23 23:33:59 +0100

    PokedexScreen  and view model

--- app/src/main/java/com/pandas/pokedexeps/ui/pokedex/PokedexScreen.kt
+++ app/src/main/java/com/pandas/pokedexeps/ui/pokedex/PokedexScreen.kt
@@ -4,17 +4,25 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pandas.pokedexeps.ui.theme.PokemonTypography
 import com.pandas.pokedexeps.ui.components.PokemonCard
+import com.pandas.pokedexeps.ui.pokedex.PokedexViewModel
 
 @Composable
-fun PokedexScreen() {
+fun PokedexScreen(viewModel: PokedexViewModel = viewModel()) {
+    val pokemonList by viewModel.pokemonList
+    val isLoading by viewModel.isLoading
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -26,23 +34,24 @@ fun PokedexScreen() {
             style = PokemonTypography.bodyLarge,
             modifier = Modifier.padding(bottom = 16.dp)
         )
-        LazyColumn {
-            items(10) { rowIndex ->
-                LazyRow(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    item {
-                        PokemonCard(id = rowIndex * 2 + 1,
-                            name = "Pokemon ${rowIndex * 2 + 1}",
-                            imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${rowIndex * 2 + 1}.png",
-                            modifier = Modifier.width(180.dp))
-                    }
-                    item {
-                        PokemonCard(id = rowIndex * 2 + 2,
-                            name = "Pokemon ${rowIndex * 2 + 2}",
-                            imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${rowIndex * 2 + 2}.png",
-                            modifier = Modifier.width(180.dp))
+        if (isLoading) {
+            Text(text = "Loading...", style = MaterialTheme.typography.bodyLarge)
+        } else {
+            LazyColumn {
+                itemsIndexed(pokemonList.chunked(2)) { _, rowPokemons ->
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Absolute.Left,
+
+                    ) {
+                        rowPokemons.forEach { pokemon ->
+                            PokemonCard(
+                                id = pokemon.id,
+                                name = pokemon.name,
+                                imageUrl = pokemon.imageUrl,
+                                modifier = Modifier.width(170.dp)
+                            )
+                        }
                     }
                 }
             }
