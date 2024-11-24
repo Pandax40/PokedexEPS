ec9b2acb3c812e334a72e6495334fc6e0a1998ec
David <davidbefi@gmail.com>
2024-11-24 03:22:21 +0100

    Update AdapterFactory.kt

--- app/src/main/java/com/pandas/pokedexeps/adapter/AdapterFactory.kt
+++ app/src/main/java/com/pandas/pokedexeps/adapter/AdapterFactory.kt
@@ -4,10 +4,17 @@ import com.pandas.pokedexeps.services.EventServiceAdapter
 import com.pandas.pokedexeps.services.IEventService
 import com.pandas.pokedexeps.services.IPokemonService
 import com.pandas.pokedexeps.services.ITeamService
+<<<<<<< Updated upstream
 import com.pandas.pokedexeps.services.IZoneService
 import com.pandas.pokedexeps.services.PokemonServiceAdapter
 import com.pandas.pokedexeps.services.TeamServiceAdapter
 import com.pandas.pokedexeps.services.ZoneServiceAdapter
+=======
+import com.pandas.pokedexeps.services.IIAService
+import com.pandas.pokedexeps.services.PokemonServiceAdapter
+import com.pandas.pokedexeps.services.TeamServiceAdapter
+import com.pandas.pokedexeps.services.IAServiceAdapter
+>>>>>>> Stashed changes
 
 
 object AdapterFactory {
@@ -20,6 +27,7 @@ object AdapterFactory {
         return TeamServiceAdapter()
     }
 
+<<<<<<< Updated upstream
     fun createZoneServiceAdapter(): IZoneService {
         return ZoneServiceAdapter()
     }
@@ -27,5 +35,9 @@ object AdapterFactory {
 
     fun createEventServiceAdapter(): IEventService {
         return EventServiceAdapter()
+=======
+    fun createIAServiceAdapter(): IIAService {
+        return IAServiceAdapter()
+>>>>>>> Stashed changes
     }
 }
